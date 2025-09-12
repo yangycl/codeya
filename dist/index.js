949f7556f168e6f692424aa96b5cdc2bdd34b5fd
yangycl <yangyangyclee@gmail.com>
2025-09-12 19:43:00 +0800

    加上 CSS

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,17 @@
 function run() {
     let html = $("#html").val();
     let js = $("#js").val();
+    var css = $("#css").val();
+    if (!html)
+        throw new Error("HTML is undefined");
+    if (!js)
+        throw new Error("JS is undefined");
+    if (!css)
+        throw new Error("CSS is undefined");
+    // 加入 CSS 到 head
+    $("head").append("<style id='dynamic-style'>" + css + "</style>");
+    // 再插入 HTML 到 body
     $("body").append(html);
+    // 最後執行 JS（這樣 JS 能操作剛剛放進來的 DOM）
     eval(js);
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 /// <reference types="jquery" />
 function run():void{
-    let html = ($("#html") as any).val();
-    let js = ($("#js") as any).val();
+    let html = ($("#html") as JQuery<HTMLTextAreaElement>).val();
+    let js = ($("#js") as JQuery<HTMLTextAreaElement>).val();
+    var css =  ($("#css")as JQuery<HTMLTextAreaElement>).val();
+    if(!html) throw new Error("HTML is undefined");
+    if(!js) throw new Error("JS is undefined");
+    if(!css) throw new Error("CSS is undefined");
+    // 加入 CSS 到 head
+    $("head").append("<style id='dynamic-style'>" + css + "</style>");
+
+    // 再插入 HTML 到 body
     $("body").append(html);
+
+    // 最後執行 JS（這樣 JS 能操作剛剛放進來的 DOM）
     eval(js);
 }
